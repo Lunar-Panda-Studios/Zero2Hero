6b1fedfde5c4a63edc7af848b45bfdd2d18a83e6
Sharpy <cam-chester@hotmail.co.uk>
2022-10-17 14:06:44 +0100

    Implemented machinegun weapon

--- Zero2Hero/Source/Zero2Hero/MachineGun.cpp
+++ Zero2Hero/Source/Zero2Hero/MachineGun.cpp
@@ -8,10 +8,51 @@ AMachineGun::AMachineGun()
 	PrimaryActorTick.bCanEverTick = true;
 }
 
+void AMachineGun::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FireLocation = FindComponentByClass<USphereComponent>();
+}
+
+void AMachineGun::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	currentCooldown += DeltaTime;
+	Attack();
+}
+
 void AMachineGun::PrimaryAttack()
 {
+	shooting = true;
 }
 
 void AMachineGun::SecondaryAttack()
 {
+}
+
+void AMachineGun::PrimaryAttackEnd()
+{
+	shooting = false;
+}
+
+void AMachineGun::Attack()
+{
+	if (shooting)
+	{
+		FActorSpawnParameters spawnParams;
+		spawnParams.Owner = this;
+		spawnParams.Instigator = GetInstigator();
+		if (currentCooldown > fireRate)
+		{
+			FRotator rotation = GetActorRotation();
+			AProjectile* Bullet = GetWorld()->SpawnActor<AProjectile>(Projectile, FireLocation->GetComponentLocation(), rotation, spawnParams);
+			if (Bullet != nullptr)
+			{
+				Bullet->Damage = Damage;
+			}
+			currentCooldown = 0.0f;
+			
+		}
+	}
 }
--- Zero2Hero/Source/Zero2Hero/MachineGun.h
+++ Zero2Hero/Source/Zero2Hero/MachineGun.h
@@ -12,12 +12,26 @@ class ZERO2HERO_API AMachineGun : public ARangedWeapon
 {
 	GENERATED_BODY()
 protected:
-
+	virtual void BeginPlay() override;
 public:
+	virtual void Tick(float DeltaTime) override;
 	AMachineGun();
+	
 	UFUNCTION(BlueprintCallable)
 		void PrimaryAttack() override;
 	UFUNCTION(BlueprintCallable)
 		void SecondaryAttack() override;
+	UFUNCTION(BlueprintCallable)
+		void PrimaryAttackEnd() override;
+	UFUNCTION()
+		void Attack();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float fireRate = 1.0f;
+	UPROPERTY()
+		float currentCooldown = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		bool shooting = false;
 
 };
