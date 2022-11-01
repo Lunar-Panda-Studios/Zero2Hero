2e20e36ffcf23b91c8dfb3842012f6a5e1479a46
Sharpy <cam-chester@hotmail.co.uk>
2022-11-01 17:48:55 +0000

    Fixing header placement

--- Zero2Hero/Source/Zero2Hero/Turret.cpp
+++ Zero2Hero/Source/Zero2Hero/Turret.cpp
@@ -1,10 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
+#include "Turret.h"
 #include "Kismet\KismetSystemLibrary.h"
 #include "Kismet/KismetMathLibrary.h"
 #include "Damageable.h"
 #include "Enemy.h"
-#include "Turret.h"
+
 
 // Sets default values
 ATurret::ATurret()
--- Zero2Hero/Source/Zero2Hero/TurretSeed.cpp
+++ Zero2Hero/Source/Zero2Hero/TurretSeed.cpp
@@ -1,8 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
+#include "TurretSeed.h"
 #include "Turret.h"
 #include "Kismet\KismetSystemLibrary.h"
-#include "TurretSeed.h"
+
 
 ATurretSeed::ATurretSeed()
 {
