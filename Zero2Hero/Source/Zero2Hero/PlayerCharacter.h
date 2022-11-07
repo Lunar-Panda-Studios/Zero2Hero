70a20be89ffe57b41a549f48e9acfeb28445354a
FilipJoz <5jozif81@solent.ac.uk>
2022-11-07 21:14:47 +0000

    Revert "Revert "Merge remote-tracking branch 'origin/GD-Eoin' into GD-Jamie""
    This reverts commit e6ebac9d4ba1c5c0ddef94480e0ab9940c72e0ec.

--- Zero2Hero/Source/Zero2Hero/PlayerCharacter.h
+++ Zero2Hero/Source/Zero2Hero/PlayerCharacter.h
@@ -148,8 +148,6 @@ protected:
 		ADialogueSystem* DialogueSystem;
 	UPROPERTY(EditAnywhere, Category = "Dialogue Settings")
 		TSubclassOf<ADialogueSystem> DialogueSystemClass;
-	UPROPERTY()
-		bool Allow = true;
   UPROPERTY()
 		UCharacterMovementComponent* characterMovementComp;
 
@@ -278,4 +276,10 @@ public:
 		void StopWallJump();
 	UFUNCTION(BlueprintImplementableEvent)
 		void StartWallJump();
+
+	UFUNCTION()
+		void DropExcessAmmo();
+
+	UFUNCTION(BlueprintImplementableEvent)
+		void GrappleEnd();
 };
