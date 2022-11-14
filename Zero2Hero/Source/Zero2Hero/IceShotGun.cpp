69806b3057437abfbd3a4f0e9baf6dfa742b0a0d
Sharpy <cam-chester@hotmail.co.uk>
2022-11-14 11:28:21 +0000

    Added verticality to Ice Shotgun

--- Zero2Hero/Source/Zero2Hero/IceShotGun.cpp
+++ Zero2Hero/Source/Zero2Hero/IceShotGun.cpp
@@ -42,14 +42,14 @@ void AIceShotGun::PrimaryAttack()
 		FRotator Temp = Camera->GetSpringArm()->GetComponentRotation();
 		Temp.Pitch += CameraAimDifference;
 
-		FRotator Max = FRotator(Temp.Pitch, Temp.Roll + DegreesAroundCentre/2, Temp.Roll);
-		FRotator Min = FRotator(Temp.Pitch, Temp.Roll - DegreesAroundCentre/2, Temp.Roll);
+		FRotator Max = FRotator(Temp.Pitch + DegreesAroundCentre / 2, Temp.Roll + DegreesAroundCentre/2, Temp.Roll);
+		FRotator Min = FRotator(Temp.Pitch - DegreesAroundCentre / 2, Temp.Roll - DegreesAroundCentre/2, Temp.Roll);
 
 		if (DecreaseCharge(ChargeUsage))
 		{
 			for (int i = 0; i < IcicleNumber; i++)
 			{
-				Rotation = FRotator(Temp.Pitch, (Temp.Yaw - DegreesAroundCentre / IcicleNumber) + FMath::FRandRange(Min.Yaw, Max.Yaw), (Temp.Roll - DegreesAroundCentre / 4) + FMath::FRandRange(Min.Roll, Max.Roll));
+				Rotation = FRotator((Temp.Pitch - DegreesAroundCentre / IcicleNumber) + FMath::FRandRange(Min.Pitch, Max.Pitch), (Temp.Yaw - DegreesAroundCentre / IcicleNumber) + FMath::FRandRange(Min.Yaw, Max.Yaw), (Temp.Roll - DegreesAroundCentre / 4) + FMath::FRandRange(Min.Roll, Max.Roll));
 				AProjectile* Icicle = GetWorld()->SpawnActor<AProjectile>(Projectile, FireLocation->GetComponentLocation(), Rotation, spawnParams);
 				if (Icicle != nullptr)
 				{
